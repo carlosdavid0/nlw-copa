f04560d47db743dcb8da7af1042df1b2252f9c39
carlosdavid <romao.carlosdavi@gmail.com>
2022-11-01 17:41:22 -0300

    :sparkles: mobile setup

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,14 @@
+import { Center, StatusBar, Text } from 'native-base'
+import React from 'react'
+
+export function SignIn() {
+    return (
+        <Center flex={1} bg='black' alignItems={'center'} justifyContent={'center'}>
+            <Text color={'gray.200'} fontSize={24}>
+                React Native
+            </Text>
+
+        </Center>
+
+    )
+}
